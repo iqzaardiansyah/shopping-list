29cf067f44dba690f8d02a1bdf479d02331cff61
iqzaardiansyah <iqzaardiansyah@gmail.com>
2023-09-23 19:43:24 +0700

    update mengikuti announcement

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
 from main.models import Product
+from django import forms
+from django.contrib.auth.models import User
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "price", "description"]
+        fields = ["name", "price", "description", "user"]
+    user = forms.ModelChoiceField(queryset=User.objects.all(), required=True)
