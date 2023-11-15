873ff9f2b4820b0c163c38bc417e0d50cc3813cd
iqzaardiansyah <iqzaardiansyah@gmail.com>
2023-11-15 12:44:56 +0700

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login
+from .views import *
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
